43c04a9fce6b7bbd46d31931275ad60095bb7d96
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-13 18:31:07 +0300

    first commit

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  let className = "";
+  let iconName = "";
+  let iconType: "filled" | "outlined" | "round" | "sharp" | "two-tone" =
+    "outlined";
+  let style = "";
+  export { className as class, iconName as name, iconType as type, style };
+</script>
+
+<i
+  translate="no"
+  class="select-none material-icons {iconType} {className}"
+  {style}
+>
+  {iconName}
+</i>
+
+<style>
+  @font-face {
+    font-family: "Material Icons";
+    font-style: normal;
+    font-weight: 400;
+    src: url(https://example.com/MaterialIcons-Regular.eot); /* For IE6-8 */
+    src: local("Material Icons"), local("MaterialIcons-Regular"),
+      url(https://example.com/MaterialIcons-Regular.woff2) format("woff2"),
+      url(https://example.com/MaterialIcons-Regular.woff) format("woff"),
+      url(https://example.com/MaterialIcons-Regular.ttf) format("truetype");
+  }
+  .select-none {
+    user-select: none;
+  }
+  .material-icons {
+    font-family: "Material Icons";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 24px; /* Preferred icon size */
+    display: inline-block;
+    line-height: 1;
+    text-transform: none;
+    letter-spacing: normal;
+    word-wrap: normal;
+    white-space: nowrap;
+    direction: ltr;
+
+    /* Support for all WebKit browsers. */
+    -webkit-font-smoothing: antialiased;
+    /* Support for Safari and Chrome. */
+    text-rendering: optimizeLegibility;
+
+    /* Support for Firefox. */
+    -moz-osx-font-smoothing: grayscale;
+
+    /* Support for IE. */
+    font-feature-settings: "liga";
+  }
+  .material-icons.filled {
+    font-family: "Material Icons";
+  }
+  .material-icons.outlined {
+    font-family: "Material Icons Outlined";
+  }
+  .material-icons.round {
+    font-family: "Material Icons Round";
+  }
+  .material-icons.sharp {
+    font-family: "Material Icons Sharp";
+  }
+  .material-icons.two-tone {
+    font-family: "Material Icons Two Tone";
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import Icon from "$lib/Icon.svelte";
+</script>
+
+<div class="wrapper">
+  <div class="show">
+    <Icon name="person" type="filled"/>
+    <span>Filled</span>
+  </div>
+  <div class="show">
+    <Icon name="person" type="outlined" />
+    <span>Outlined</span>
+  </div>
+
+  <div class="show">
+    <Icon name="person" type="round" />
+    <span>Round</span>
+  </div>
+
+  <div class="show">
+    <Icon name="person" type="sharp" />
+    <span>Sharp</span>
+  </div>
+
+  <div class="show">
+    <Icon name="person" type="two-tone" />
+    <span>Two Tone</span>
+  </div>
+</div>
+
+<style global>
+  .wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .show {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 1rem;
+    font-size: large;
+  }
+
+  .show span {
+    margin-top: 1rem;
+  }
+</style>
