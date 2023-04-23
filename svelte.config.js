b8d10855407229b1a22d89276af2d770d902a3ac
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-24 01:57:27 +0300

    refactor: deploy github static for material icons font

--- svelte.config.js
+++ svelte.config.js
@@ -5,23 +5,23 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
+    kit: {
         adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
             pages: 'build',
             assets: 'build',
-            fallback: null,
+            fallback: "index.html",
             precompress: false,
             strict: true,
         }),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		},
+        paths: {
+            base: process.env.NODE_ENV === "production" ? "/meticon" : "",
+        },
     }
 };
 
