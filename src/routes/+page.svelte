bf751ca8cdd1d6e8f3259a8a766632988b836295
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-24 02:51:32 +0300

    refactor: deploy github static for material icons font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,30 @@
 <script lang="ts">
   import Icon from "$lib/Icon.svelte";
+
+  const og_image = "https://opengraph.githubassets.com/a974346ea8702243b4e5892f20f0017ccf0072fac3962c6ba39ed85bbdd9da5e/ugurkorkmaz/meticon";
 </script>
+
+<svelte:head>
+  <title>Meticon</title>
+  <meta name="twitter:image:src" content={og_image} />
+  <meta name="twitter:site" content="@ugurkorkmaz" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="ugurkorkmaz/meticon: A Google material icon library for Svelte" />
+  <meta name="twitter:description" content="A Google material icon library for Svelte. Contribute to ugurkorkmaz/meticon development by creating an account on GitHub." />
+  <meta property="og:image" content={og_image} />
+  <meta property="og:image:alt" content="A Google material icon library for Svelte. Contribute to ugurkorkmaz/meticon development by creating an account on GitHub." />
+  <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="600" />
+  <meta property="og:site_name" content="Meticon" />
+  <meta property="og:type" content="object" />
+  <meta property="og:title" content="ugurkorkmaz/meticon: A Google material icon library for Svelte" />
+  <meta property="og:url" content="https://meticon.ugurkorkmaz.net" />
+  <meta property="og:description" content="A Google material icon library for Svelte. Contribute to ugurkorkmaz/meticon development by creating an account on GitHub." />
+</svelte:head>
 
 <div class="wrapper">
   <div class="show">
-    <Icon name="person" type="filled"/>
+    <Icon name="person" type="filled" />
     <span>Filled</span>
   </div>
   <div class="show">
@@ -36,6 +56,7 @@
     align-items: center;
     height: 100vh;
   }
+
   .show {
     display: flex;
     flex-direction: column;
--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: "index.html",
-            precompress: false,
-            strict: true,
-        }),
-        paths: {
-            base: process.env.NODE_ENV === "production" ? "/meticon" : "",
-        },
+        adapter: adapter(),
     }
 };
 
