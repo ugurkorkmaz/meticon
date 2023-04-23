205212f0693c755e24ec4e96533edf14160ed8f3
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-24 01:56:25 +0300

    Updates

---
+++ _app/immutable/chunks/1.0542237b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.7e3f47d0.js";export{t as component};
---
+++ _app/immutable/chunks/2.c1d0132a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.01e9a717.js";export{t as component};
