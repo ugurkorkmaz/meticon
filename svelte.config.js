81d943de4aa490c29dab5f775ef5829c3452ef88
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-04-24 22:03:08 +0300

    feat: add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-    kit: {
-        adapter: adapter(),
-    }
+  kit: {
+    adapter: adapter({}),
+  },
 };
 
 export default config;
